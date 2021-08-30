66e5aa04235a5f425736eca30a4f8b608ba3bd90
Your Name <you@example.com>
2021-08-30 12:46:33 +0600

    logic of work

--- scrypt/scrypt.py
+++ scrypt/scrypt.py
@@ -19,14 +19,15 @@ def pull_branch(source_repo_adress, source_branch, sourse_id):
     # os.system(f'cd {project_folder}')
     # os.system(f'git clone {source_repo_adress}')
     # os.system(f'git remote add s{sourse_id} {source_repo_adress}')
-    os.system(f'git pull s{sourse_id}')
+    os.system(f'git pull s{sourse_id} {source_branch}')
     os.system(f'git fetch s{sourse_id}')
-    os.system(f'git checkout {source_branch}')
+    
+    os.system(f'git checkout --track s{sourse_id}/{source_branch}')
 
 
 def push_branch(destination_repo_adress, destination_branch, destination_id):
-    os.system(f'git remote add client {destination_repo_adress}')
-    os.system(f'git pull d{destination_id}')
+    os.system(f'git remote add d{destination_id} {destination_repo_adress}')
+    # os.system(f'git pull d{destination_id}')
     os.system(f'git fetch d{destination_id}')
     os.system(f'git branch -M {destination_branch}')
     os.system(f'git push d{destination_id} {destination_branch}')
